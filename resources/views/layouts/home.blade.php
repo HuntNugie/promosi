0169bbbe43aa637e66d0497ee103d78d92960c55
Nugie kurniawan <nugiebinhar@gmail.com>
2025-06-06 21:53:59 +0700

    Memisahkan component component

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+
+    <x-head></x-head>
+
+
+</head>
+
+<body class="index-page">
+
+    <x-navigation.header/>
+
+  <main class="main">
+
+    <!-- Hero Section -->
+    <x-hero></x-hero>
+    <!-- /Hero Section -->
+
+    <!-- About Section -->
+   <x-about></x-about>
+    <!-- /About Section -->
+
+    <!-- Stats Section -->
+   <x-stats></x-stats>
+    <!-- /Stats Section -->
+
+    <!-- Services Section -->
+  <x-service></x-service>
+    <!-- /Services Section -->
+
+    <!-- Testimonials Section -->
+    <x-testi></x-testi>
+    <!-- /Testimonials Section -->
+
+
+
+    <!-- product Section -->
+    <x-product></x-product>
+  <!-- /product Section -->
+
+    <!-- Team Section -->
+    <x-team></x-team>
+   <!-- /Team Section -->
+
+    <!-- Pricing Section -->
+    <x-pricing></x-pricing>
+ <!-- /Pricing Section -->
+
+
+    <!-- Contact Section -->
+    <x-contact></x-contact>
+    <!-- /Contact Section -->
+
+  </main>
+
+ <x-footer></x-footer>
+
+  <!-- Scroll Top -->
+  <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Preloader -->
+  <div id="preloader">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+
+  <!-- Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
