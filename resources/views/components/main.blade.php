8403013ef5f1904c4b4569a005e4988352b541a8
Nugie kurniawan <nugiebinhar@gmail.com>
2025-06-06 22:17:06 +0700

    Memasukkan component component ke component main

---
+++ resources/views/components/main.blade.php
@@ -0,0 +1,40 @@
+<div>
+    <!-- Hero Section -->
+    <x-hero></x-hero>
+    <!-- /Hero Section -->
+
+    <!-- About Section -->
+   <x-about></x-about>
+    <!-- /About Section -->
+
+    <!-- Stats Section -->
+   <x-stats></x-stats>
+    <!-- /Stats Section -->
+
+    <!-- Services Section -->
+  <x-service></x-service>
+    <!-- /Services Section -->
+
+    <!-- Testimonials Section -->
+    <x-testi></x-testi>
+    <!-- /Testimonials Section -->
+
+
+
+    <!-- product Section -->
+    <x-product></x-product>
+  <!-- /product Section -->
+
+    <!-- Team Section -->
+    <x-team></x-team>
+   <!-- /Team Section -->
+
+    <!-- Pricing Section -->
+    <x-pricing></x-pricing>
+ <!-- /Pricing Section -->
+
+
+    <!-- Contact Section -->
+    <x-contact></x-contact>
+    <!-- /Contact Section -->
+</div>
--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -15,44 +15,7 @@
 
   <main class="main">
 
-    <!-- Hero Section -->
-    <x-hero></x-hero>
-    <!-- /Hero Section -->
-
-    <!-- About Section -->
-   <x-about></x-about>
-    <!-- /About Section -->
-
-    <!-- Stats Section -->
-   <x-stats></x-stats>
-    <!-- /Stats Section -->
-
-    <!-- Services Section -->
-  <x-service></x-service>
-    <!-- /Services Section -->
-
-    <!-- Testimonials Section -->
-    <x-testi></x-testi>
-    <!-- /Testimonials Section -->
-
-
-
-    <!-- product Section -->
-    <x-product></x-product>
-  <!-- /product Section -->
-
-    <!-- Team Section -->
-    <x-team></x-team>
-   <!-- /Team Section -->
-
-    <!-- Pricing Section -->
-    <x-pricing></x-pricing>
- <!-- /Pricing Section -->
-
-
-    <!-- Contact Section -->
-    <x-contact></x-contact>
-    <!-- /Contact Section -->
+    <x-main></x-main>
 
   </main>
 
