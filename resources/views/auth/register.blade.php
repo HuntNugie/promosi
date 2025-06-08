d279d7028234ef6a9089923809aad868765fa9c7
Nugie kurniawan <nugiebinhar@gmail.com>
2025-06-08 17:52:50 +0700

    Merubah tampilan dari register serta merubah redirect logout menjadi ke login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('auth')
-<div class="container">
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -73,5 +73,98 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
+   <div class="container-fluid page-body-wrapper full-page-wrapper d-flex">
+      <div class="content-wrapper d-flex align-items-stretch auth auth-img-bg">
+        <div class="row flex-grow">
+          <div class="col-lg-6 d-flex align-items-center justify-content-center">
+            <div class="auth-form-transparent text-left p-3">
+              <div class="brand-logo">
+                <img src="{{ asset('admin/images/logo.svg') }}" alt="logo">
+              </div>
+              <h4>Registrasi Admin Ngk Company</h4>
+              <h6 class="font-weight-light">Daftar registrasi admin baru</h6>
+              <form class="pt-3" action="{{ route("register") }}" method="post">
+                @csrf
+                <div class="form-group">
+                  <label>Nama Lengkap</label>
+                  <div class="input-group">
+                    <div class="input-group-prepend bg-transparent">
+                      <span class="input-group-text bg-transparent border-right-0">
+                        <i class="mdi mdi-account-outline text-primary"></i>
+                      </span>
+                    </div>
+                    <input type="text" name="name" class="form-control form-control-lg border-left-0 @error('name')
+                        is-invalid
+                    @enderror" placeholder="Jhon Doe" value="{{ old('name') }}" required>
+                      @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                       @enderror
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label>Email</label>
+                  <div class="input-group">
+                    <div class="input-group-prepend bg-transparent">
+                      <span class="input-group-text bg-transparent border-right-0">
+                        <i class="mdi mdi-email-outline text-primary"></i>
+                      </span>
+                    </div>
+                    <input type="email" name="email" class="form-control form-control-lg border-left-0 @error('email')
+                        is-invalid
+                    @enderror" placeholder="Example@example.com" value="{{ old('email') }}" required>
+                    @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                       @enderror
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label>Password</label>
+                  <div class="input-group">
+                    <div class="input-group-prepend bg-transparent">
+                      <span class="input-group-text bg-transparent border-right-0">
+                        <i class="mdi mdi-lock-outline text-primary"></i>
+                      </span>
+                    </div>
+                    <input type="password" class="form-control form-control-lg border-left-0 @error('password')
+                        is-invalid
+                    @enderror" id="exampleInputPassword" name="password" placeholder="Password" required>
+                    @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                       @enderror
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label>Konfirmasi Password</label>
+                  <div class="input-group">
+                    <div class="input-group-prepend bg-transparent">
+                      <span class="input-group-text bg-transparent border-right-0">
+                         <i class="mdi mdi-lock-outline text-primary"></i>
+                      </span>
+                    </div>
+                     <input type="password" class="form-control form-control-lg border-left-0" id="password_confirmation" name="password_confirmation" placeholder="Password" required>
+                  </div>
+                </div>
+                <div class="mt-3">
+                  <button class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" type="submit">REGISTER</button>
+                </div>
+                <div class="text-center mt-4 font-weight-light">
+                  Sudah mempunyai akun admin? <a href="{{ route("login") }}" class="text-primary">Login</a>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div class="col-lg-6 register-half-bg d-none d-lg-flex flex-row">
+            <p class="text-white font-weight-medium text-center flex-grow align-self-end">Copyright &copy; 2018  All rights reserved.</p>
+          </div>
+        </div>
+      </div>
+      <!-- content-wrapper ends -->
+    </div>
 @endsection
